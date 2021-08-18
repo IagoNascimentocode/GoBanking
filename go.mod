3bcf5c878c58cfa5c3a48cdd8accd63ae82bae7f
IagoNascimentocode <iago_ns@hotmail.com>
2021-08-17 21:37:33 -0300

    encripted password

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- src/accounts/services/CreateAccount.go
+++ src/accounts/services/CreateAccount.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"gitlab.com/IagoNascimentocode/StoneBanking/src/accounts/models"
 	"gitlab.com/IagoNascimentocode/StoneBanking/src/database"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func CrerateAccounts(c *gin.Context) {
@@ -17,7 +18,9 @@ func CrerateAccounts(c *gin.Context) {
 		return
 	}
 
-	account := models.Account{Name: input.Name, Secret: input.Secret, Cpf: input.Cpf}
+	hash, _ := bcrypt.GenerateFromPassword([]byte(input.Secret), 8)
+
+	account := models.Account{Name: input.Name, Cpf: input.Cpf, Secret: string(hash)}
 
 	database.DB.Create(&account)
 
